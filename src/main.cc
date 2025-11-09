889130b9bfbbac3cf7f3a623bd61cf18d21188e8
Semyon <zhigunovsemen@gmail.com>
2025-11-09 18:13:45 +0300

    Сделал задание по AVX

--- src/main.cc
+++ src/main.cc
@@ -13,105 +13,23 @@ typedef uint16_t u16;
 typedef int32_t i32;
 typedef uint32_t u32;
 
-static void sse_i32_sub()
-{
-	using type = i32;
-	puts("Задание 2. u16 C = A - B");
-	type a[8] = {0, 1, std::numeric_limits<type>::min(), 11, 256, 4, 99, 0};
-	type b[8] = {1, 38, 4, 211, 0, 2, 19, std::numeric_limits<type>::max()};
-	type c[8] = {};
-
-	printf("a: ");
-	print_array(std::span{a, 8});
-	printf("b: ");
-	print_array(std::span{b, 8});
-
-	_asm {
-		movdqu xmm0, a
-		movdqu xmm1, b
-		psubw xmm0, xmm1
-		movdqu c, xmm0
-	}
-
-	printf("c: ");
-	print_array(std::span{c, 8});
-}
-
-static void sse_i8_sum()
-{
-	using type = i8;
-	puts("Задание 3. i8 C = A + B");
-	type a[32] = "Этот текст из 32 Б.";
-	type b[32] = "This string has just 2^5 bytes.";
-	type c[32] = {};
-
-	printf("a: ");
-	print_array(std::span{a, 32});
-	printf("b: ");
-	print_array(std::span{b, 32});
-
-	_asm {
-		movdqu xmm0, a
-		movdqu xmm1, b
-		paddb xmm0, xmm1
-		movdqu c, xmm0
-	}
-
-	printf("c: ");
-	print_array(std::span{c, 16});
-}
-
-static void sse_u16_sum()
-{
-	using type = u16;
-	puts("Задание 3. i8 C = A + B");
-	type a[16] = {0,   1,	 std::numeric_limits<type>::max(),
-		      11,  220, 4,
-		      99,  1,	 12,
-		      177, 11,	 12,
-		      13, 14,	 88};
-	type b[16] = {1,   38,	4,   125,
-		      0,   2,	19,  std::numeric_limits<type>::min(),
-		      99, 100, 111, 112,
-		      26,  128, 66};
-	type c[16] = {};
-
-	printf("a: ");
-	print_array(std::span{a, 16});
-	printf("b: ");
-	print_array(std::span{b, 16});
-
-	_asm {
-		movdqu xmm0, a
-		movdqu xmm1, b
-		paddb xmm0, xmm1
-		movdqu c, xmm0
-	}
-
-	printf("c: ");
-	print_array(std::span{c, 16});
-}
-
 static void sse_f64_mult()
 {
 	using type = f64;
-	puts("Задание 4. f32 C = A * B");
-	type a[4] = {3.5f, 1.1f, -std::numeric_limits<type>::infinity(),
-		     -10.0f};
-	type b[4] = {0.5f, -5.1f, -0.1f, 12.3f};
-	type c[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+	puts("Задание 1. f64 C = A * B");
+	type a[4] = {3.5, 111.0, -std::numeric_limits<type>::infinity(), -10.0};
+	type b[4] = {0.5, std::numeric_limits<type>::min(), -0.1, 12.3};
+	type c[4] = {0.0, 0.0, 0.0, 0.0};
 
 	printf("a: ");
 	print_array(std::span{a, 4});
 	printf("b: ");
 	print_array(std::span{b, 4});
 
-	_asm {
-		movups xmm0, a
-		movups xmm1, b
-		mulps xmm0, xmm1
-		movups c, xmm0
-	}
+	auto vec_a = _mm256_load_pd(a);
+	auto vec_b = _mm256_load_pd(b);
+	auto vec_c = _mm256_mul_pd(vec_a, vec_b);
+	_mm256_store_pd(c, vec_c);
 
 	printf("c: ");
 	print_array(std::span{c, 4});
@@ -119,8 +37,8 @@ static void sse_f64_mult()
 
 static void sse_f32_div()
 {
-	using type = f64;
-	puts("Задание 5. f64 C = A / B");
+	using type = f32;
+	puts("Задание 2. f32 C = A / B");
 	type a[8] = {
 		1.1f,	13.37f, 3.5f, 1.0f,
 		-10.0f, 8.8f,	0.0f, std::numeric_limits<type>::quiet_NaN()};
@@ -128,7 +46,7 @@ static void sse_f32_div()
 	type b[8] = {2.28f,
 		     54.6f,
 		     0.0f,
-		     99999.9f,
+		     99099.9f,
 		     -0.0f,
 		     std::numeric_limits<type>::max(),
 		     std::numeric_limits<type>::min(),
@@ -136,31 +54,21 @@ static void sse_f32_div()
 	type c[8] = {};
 
 	printf("a: ");
-	print_array(std::span{a, 2});
+	print_array(std::span{a, 8});
 	printf("b: ");
-	print_array(std::span{b, 2});
+	print_array(std::span{b, 8});
 
-	_asm {
-		movupd xmm0, a
-		movupd xmm1, b
-		divpd xmm0, xmm1
-		movupd c, xmm0
-	}
+	auto vec_a = _mm256_load_ps(a);
+	auto vec_b = _mm256_load_ps(b);
+	auto vec_c = _mm256_div_ps(vec_a, vec_b);
+	_mm256_store_ps(c, vec_c);
 
 	printf("c: ");
-	print_array(std::span{c, 2});
+	print_array(std::span{c, 8});
 }
 
 int main()
 {
-	sse_i32_sub();
-	std::println();
-
-	sse_u16_sum();
-	std::println();
-
-	sse_i8_sum();
-	std::println();
 
 	sse_f32_div();
 	std::println();
